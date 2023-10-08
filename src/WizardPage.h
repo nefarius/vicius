390511fc629475cb8d2cc27c06a0fd384d66d86f
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-10-08 16:53:25 +0200

    Added docs

--- src/WizardPage.h
+++ src/WizardPage.h
@@ -2,8 +2,25 @@
 
 enum class WizardPage
 {
+	/**
+	 * \brief The first page with basic actions the user can choose.
+	 */
 	Start = 0,
+	/**
+	 * \brief USer agreed to download and install right now.
+	 */
 	DownloadAndInstall,
+	/**
+	 * \brief Single update available, displays single summary.
+	 */
+	SingleVersionSummary,
+	/**
+	 * \brief Multiple new version available, display list and details.
+	 */
+	MultipleVersionsOverview,
+	/**
+	 * \brief The last possible wizard page.
+	 */
 	End
 };
 
