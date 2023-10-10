ef434b81483578698d0d6385477b35ef65f73c8e
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-10-10 18:58:33 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -240,32 +240,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 			window.close();
 		}
 
-		/*
-		// icons example
-		ImGui::Button(ICON_FK_SEARCH " Search");
-		ImGui::Button(ICON_FK_ARROW_LEFT);
-
-		// example markdown
-		const auto markdownText = u8R"(
-# H1 Header: Text and Links
-You can add [links like this one to enkisoftware](https://www.enkisoftware.com/) and lines will wrap well.
-You can also insert images ![image alt text](image identifier e.g. filename)
-Horizontal rules:
-***
-___
-*Emphasis* and **strong emphasis** change the appearance of the text.
-## H2 Header: indented text.
-  This text has an indent (two leading spaces).
-	This one has two.
-### H3 Header: Lists
-  * Unordered lists
-	* Lists can be indented with two extra spaces.
-  * Lists can have [links like this one to Avoyd](https://www.avoyd.com/) and *emphasized text*
-)";
-
-		RenderChangelog(std::string((const char*)markdownText));
-		*/
-
 		ImGui::End();
 
 		window.clear();
