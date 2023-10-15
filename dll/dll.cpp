1d6c01aef37a381461730843fbb5f38a930bea3d
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-10-15 05:36:25 +0200

    Finished self-updater helper

--- dll/dll.cpp
+++ dll/dll.cpp
@@ -13,13 +13,6 @@ static std::string ConvertWideToANSI(const std::wstring& wstr)
 	return str;
 }
 
-static std::wstring ConvertAnsiToWide(const std::string& str)
-{
-	int count = MultiByteToWideChar(CP_ACP, 0, str.c_str(), static_cast<int>(str.length()), nullptr, 0);
-	std::wstring wstr(count, 0);
-	MultiByteToWideChar(CP_ACP, 0, str.c_str(), static_cast<int>(str.length()), &wstr[0], count);
-	return wstr;
-}
 
 EXTERN_C DLL_API void CALLBACK PerformUpdate(HWND hwnd, HINSTANCE hinst, LPSTR lpszCmdLine, int nCmdShow)
 {
@@ -70,18 +63,45 @@ EXTERN_C DLL_API void CALLBACK PerformUpdate(HWND hwnd, HINSTANCE hinst, LPSTR l
 		return;
 
 	std::filesystem::path original = path;
-	std::filesystem::path temp = original.parent_path() / "newupdater.exe";
+	// TEMP directory
+	std::string tempPath(MAX_PATH, '\0');
+	GetTempPathA(MAX_PATH, tempPath.data());
+	// temporary file
+	std::string tempFile(MAX_PATH, '\0');
+	GetTempFileNameA(tempPath.c_str(), "VICIUS", 0, tempFile.data());
 	curlpp::Cleanup myCleanup;
+	HANDLE hProcess = nullptr;
+	int retries = 10;
+
+	// wait until parent is no more
+	do
+	{
+		if (hProcess) CloseHandle(hProcess);
+
+		// we failed and bail since the rest of the logic will not work
+		if (--retries < 1) return;
+
+		hProcess = OpenProcess(
+			PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,
+			FALSE,
+			pid
+		);
+
+		if (hProcess) Sleep(25);
+	}
+	while (hProcess);
 
 	try
 	{
-		MoveFileA(original.string().c_str(), temp.string().c_str());
+		// we can not yet directly write to it but move it to free the original name!
+		MoveFileA(original.string().c_str(), tempFile.c_str());
 
 		// download directly to main file stream
 		std::ofstream outStream(original, std::ios::binary | std::ofstream::ate);
 		outStream << curlpp::options::Url(url);
 
-		DeleteFileA(temp.string().c_str());
+		// delete the file where this ADS sits in (yes, it works!)
+		DeleteFileA(tempFile.c_str());
 	}
 	catch (curlpp::RuntimeError& e)
 	{
