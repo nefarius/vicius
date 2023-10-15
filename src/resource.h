bbe2da5e67ae7d86a38a1f786e78b5179338ab7d
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-10-15 14:45:44 +0200

    Added self-updater as DLL resource

--- src/resource.h
+++ src/resource.h
@@ -7,12 +7,13 @@
 #define IDR_FONT_RUDA_REGULAR           103
 #define IDR_FONT_FK                     104
 #define IDS_STRING_SERVER_URL           105
+#define IDR_DLL_SELF_UPDATER            106
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
