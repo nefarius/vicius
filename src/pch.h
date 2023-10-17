20eca5397af78143905451583ee8043f4da147e6
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-10-17 14:11:13 +0200

    Restructuring includes

--- src/pch.h
+++ src/pch.h
@@ -1,1 +1,48 @@
 #pragma once
+
+// 
+// WinAPI
+// 
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <tchar.h>
+#include <Dwmapi.h>
+#include <shellapi.h>
+#include <winhttp.h>
+#include <comdef.h>
+#include <ole2.h>
+#include <taskschd.h>
+
+// 
+// ImGui, Fonts
+// 
+#include "imgui.h"
+#include "imgui-SFML.h"
+#include "imgui_markdown.h"
+#include "IconsForkAwesome.h"
+
+// 
+// SFML
+// 
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+
+//
+// Utility packages
+// 
+#include <argh.h>
+#include <neargye/semver.hpp>
+#include <magic_enum.hpp>
+#include <winreg/WinReg.hpp>
+#include <hash-library/md5.h>
+#include <hash-library/sha1.h>
+#include <hash-library/sha256.h>
+
+//
+// STL
+// 
+#include <filesystem>
+#include <tuple>
+#include <random>
