3c81e75a6e32c1c70353737e5d3d735f4248d41c
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-10-17 14:27:15 +0200

    Update Common.h

--- src/Common.h
+++ src/Common.h
@@ -1,50 +1,6 @@
 #pragma once
 
 // 
-// WinAPI
-// 
-#define NOMINMAX
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include <Dwmapi.h>
-#include <shellapi.h>
-
-// 
-// ImGui, Fonts
-// 
-#include "imgui.h"
-#include "imgui-SFML.h"
-#include "imgui_markdown.h"
-#include "IconsForkAwesome.h"
-
-// 
-// SFML
-// 
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/System/Clock.hpp>
-#include <SFML/Window/Event.hpp>
-
-//
-// STL
-// 
-#include <filesystem>
-
-// 
-// CLI parser
-// 
-#include <argh.h>
-
-//
-// Util
-// 
-#include <neargye/semver.hpp>
-
-//
-// Logging
-// 
-#include <spdlog/spdlog.h>
-
-// 
 // Custom
 // 
 #include "UniUtil.h"
