fecc2e738ce09c6414edc184693c5a88c3a3c598
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-10-24 00:47:11 +0200

    Update Updates.cs

--- examples/server/Endpoints/Updates.cs
+++ examples/server/Endpoints/Updates.cs
@@ -2,7 +2,11 @@
 
 using Nefarius.Vicius.Example.Server.Models;
 
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
 using NJsonSchema;
+using NJsonSchema.Generation;
 
 namespace Nefarius.Vicius.Example.Server.Endpoints;
 
@@ -16,7 +20,16 @@ public sealed class Updates : EndpointWithoutRequest
 
     public override async Task HandleAsync(CancellationToken ct)
     {
-        JsonSchema? schema = JsonSchema.FromType<UpdateResponse>();
+        JsonSchemaGeneratorSettings settings = new JsonSchemaGeneratorSettings()
+        {
+            ActualSerializerSettings =
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            }
+        };
+        JsonSchemaGenerator generator = new JsonSchemaGenerator(settings);
+        JsonSchema? schema = generator.Generate(typeof(UpdateResponse));
         string? schemaData = schema.ToJson();
 
         await SendStringAsync(schemaData, contentType: "application/json", cancellation: ct);
