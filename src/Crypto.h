7f1f91b2ef076d1d6be0bbe93b1f5c9a52839753
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-11-12 12:30:51 +0100

    Code re-structuring

--- src/Crypto.h
+++ src/Crypto.h
@@ -2,33 +2,5 @@
 
 namespace crypto
 {
-    typedef struct
-    {
-        LPWSTR lpszProgramName;
-        LPWSTR lpszPublisherLink;
-        LPWSTR lpszMoreInfoLink;
-    } SPROG_PUBLISHERINFO, *PSPROG_PUBLISHERINFO;
-
-    BOOL
-    GetProgAndPublisherInfo(
-        PCMSG_SIGNER_INFO pSignerInfo,
-        PSPROG_PUBLISHERINFO Info
-    );
-
-    BOOL
-    GetDateOfTimeStamp(
-        PCMSG_SIGNER_INFO pSignerInfo,
-        SYSTEMTIME* st
-    );
-
-    BOOL
-    PrintCertificateInfo(
-        PCCERT_CONTEXT pCertContext
-    );
-
-    BOOL
-    GetTimeStampSignerInfo(
-        PCMSG_SIGNER_INFO pSignerInfo,
-        PCMSG_SIGNER_INFO* pCounterSignerInfo
-    );
+    
 }
