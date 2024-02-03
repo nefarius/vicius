667078b81db66e67b3a6b1261947fc85511e8ca5
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-02-04 00:39:55 +0100

    Fixed hyperlink color contrast
    Removed unused code

--- src/markdown.cpp
+++ src/markdown.cpp
@@ -17,71 +17,6 @@ ImFont* G_Font_Default = nullptr;
 static std::map<std::string, std::shared_ptr<sf::Texture>> G_ImageTextures;
 
 
-#if 0
-// ReSharper disable once CppPassValueParameterByConstReference
-static void LinkClickedCallback(ImGui::MarkdownLinkCallbackData data)
-{
-    const std::string url(data.link, data.linkLength);
-	if (!data.isImage)
-	{
-		ShellExecuteA(nullptr, "open", url.c_str(), nullptr, nullptr, SW_SHOWNORMAL);
-	}
-}
-
-// ReSharper disable once CppParameterMayBeConst
-static void FormatChangelogCallback(const ImGui::MarkdownFormatInfo& markdownFormatInfo, bool start)
-{
-	defaultMarkdownFormatCallback(markdownFormatInfo, start);
-
-	switch (markdownFormatInfo.type)
-	{
-	// example: change the colour of heading level 2
-	case ImGui::MarkdownFormatType::HEADING:
-		{
-			if (markdownFormatInfo.level == 2)
-			{
-				if (start)
-				{
-					ImGui::PushStyleColor(ImGuiCol_Text, ImGui::GetStyle().Colors[ImGuiCol_TextDisabled]);
-				}
-				else
-				{
-					ImGui::PopStyleColor();
-				}
-			}
-			break;
-		}
-	default:
-		{
-			break;
-		}
-	}
-}
-
-inline ImGui::MarkdownImageData ImageCallback(ImGui::MarkdownLinkCallbackData data)
-{
-	// In your application you would load an image based on data_ input. Here we just use the imgui font texture.
-    const ImTextureID image = ImGui::GetIO().Fonts->TexID;
-	// > C++14 can use ImGui::MarkdownImageData imageData{ true, false, image, ImVec2( 40.0f, 20.0f ) };
-	ImGui::MarkdownImageData imageData;
-	imageData.isValid = true;
-	imageData.useLinkCallback = false;
-	imageData.user_texture_id = image;
-	imageData.size = ImVec2(40.0f, 20.0f);
-
-	// For image resize when available size.x > image width, add
-	const ImVec2 contentSize = ImGui::GetContentRegionAvail();
-	if (imageData.size.x > contentSize.x)
-	{
-		const float ratio = imageData.size.y / imageData.size.x;
-		imageData.size.x = contentSize.x;
-		imageData.size.y = contentSize.x * ratio;
-	}
-
-	return imageData;
-}
-#endif
-
 /**
  * \brief Markdown parser and render widget.
  */
@@ -107,6 +42,17 @@ struct changelog : public imgui_md
         case 0:
             return G_Font_Default;
         }
+    }
+
+    ImVec4 get_color() const override
+    {
+        if (!m_href.empty())
+        {
+            // improve hyperlink visibility
+            return ImGui::GetStyle().Colors[ImGuiCol_CheckMark];
+        }
+
+        return ImGui::GetStyle().Colors[ImGuiCol_Text];
     }
 
     void open_url() const override
