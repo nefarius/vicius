0cb5f3fbbd0efd8db261c45b47602b46b308e638
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-02-15 00:47:04 +0100

    Create RootEndpoint.cs

---
+++ examples/server/Endpoints/RootEndpoint.cs
@@ -0,0 +1,17 @@
+﻿using FastEndpoints;
+
+namespace Nefarius.Vicius.Example.Server.Endpoints;
+
+internal sealed class RootEndpoint : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Get("/");
+        AllowAnonymous();
+    }
+
+    public override Task HandleAsync(CancellationToken ct)
+    {
+        return SendRedirectAsync("https://docs.nefarius.at/projects/Vicius/", allowRemoteRedirects: true);
+    }
+}
