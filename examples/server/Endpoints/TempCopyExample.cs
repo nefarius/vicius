2f2a34771b45ae04b43c50d395d8fd5e4d75b574
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-07-22 00:27:28 +0200

    Slimmed down example

--- examples/server/Endpoints/TempCopyExample.cs
+++ examples/server/Endpoints/TempCopyExample.cs
@@ -5,7 +5,7 @@ using Nefarius.Vicius.Abstractions.Models;
 namespace Nefarius.Vicius.Example.Server.Endpoints;
 
 /// <summary>
-///     Demos a simple single release update configuration with registry-based product version detection.
+///     Demos behavioral changes when <see cref="SharedConfig.RunAsTemporaryCopy" /> is set.
 /// </summary>
 internal sealed class TempCopyExample : EndpointWithoutRequest
 {
@@ -27,7 +27,7 @@ internal sealed class TempCopyExample : EndpointWithoutRequest
                     /*
                      just an example URL; if you patch the updater with a different type of
                      executable, obviously you would brick the remote installation ;)
-                     */ 
+                     */
                     LatestUrl = "https://downloads.nefarius.at/other/nefarius/vpatch/vpatch.exe"
                 },
             Shared = new SharedConfig
@@ -51,35 +51,14 @@ internal sealed class TempCopyExample : EndpointWithoutRequest
                     Version = System.Version.Parse("7.0.14"),
                     // Markdown support in summary (changelog, description)
                     Summary = """
-                              ## Features
-                                * Awesome new thing
-                                * Also this
-                                * And that
-                                
-                              ## Bugfixes
-                                * Removed bug I put there
-                                * Whoops, that wasn't supposed to happen
-                                
-                              ## Nonsense
-                                * Need to see
-                                * What happens...
-                                * If we need scroll bars
-                                * Vertically
-                                * [Links are also supported!](https://example.org)
+                              ## Heading
+                              Fill me with Markdown!
                               """,
                     // pulling bigger (~50MB) .NET runtime setup as an example to demo progress bar
                     DownloadUrl =
                         "https://download.visualstudio.microsoft.com/download/pr/f9ea536d-8e1f-4247-88b8-e79e33fa0873/c06e39f73a3bb1ec8833bb1cde98fce3/windowsdesktop-runtime-7.0.12-win-x64.exe",
                     LaunchArguments = "/norestart",
-                    ExitCode = new ExitCodeCheck
-                    {
-                        SuccessCodes =
-                        {
-                            0, // regular success
-                            3010, // success, but reboot required
-                            // 1602 // failure (user cancelled) - you normally wouldn't want that as a success code, just an example
-                        }
-                    }
+                    ExitCode = new ExitCodeCheck { SuccessCodes = { 0 } }
                 }
             }
         };
