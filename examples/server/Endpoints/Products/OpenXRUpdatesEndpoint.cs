e2234e35827d22ed9dc863d7e2a492d707914b1a
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-08-03 22:01:01 +0200

    Added another production example: https://github.com/fredemmott/OpenXR-API-Layers-GUI

---
+++ examples/server/Endpoints/Products/OpenXRUpdatesEndpoint.cs
@@ -0,0 +1,74 @@
+﻿using System.Text.RegularExpressions;
+
+using FastEndpoints;
+
+using Nefarius.Vicius.Abstractions.Models;
+using Nefarius.Vicius.Example.Server.Services;
+
+using Octokit;
+
+namespace Nefarius.Vicius.Example.Server.Endpoints.Products;
+
+/// <summary>
+///     Crafts update configuration for
+///     <a href="https://github.com/fredemmott/OpenXR-API-Layers-GUI">OpenXR API Layers GUI</a>.
+/// </summary>
+internal sealed partial class OpenXRUpdatesEndpoint(GitHubApiService githubApiService) : EndpointWithoutRequest
+{
+    [GeneratedRegex(@"^v|(?<=\D)0+(?=\d)")]
+    private static partial Regex TagStripRegex();
+
+    public override void Configure()
+    {
+        Get("api/fredemmott/OpenXR-API-Layers-GUI/updates.json");
+        AllowAnonymous();
+        Options(x => x.WithTags("Production"));
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        IEnumerable<Release>? releases = await githubApiService.GetAllReleases("fredemmott", "OpenXR-API-Layers-GUI");
+
+        if (releases is null)
+        {
+            await SendNotFoundAsync(ct);
+            return;
+        }
+
+        UpdateResponse response = new()
+        {
+            Shared = new SharedConfig
+            {
+                ProductName = "OpenXR API Layers GUI",
+                WindowTitle = "OpenXR API Layers GUI Update",
+                RunAsTemporaryCopy = true
+            },
+            Releases = releases
+                .Select(release =>
+                {
+                    ReleaseAsset? asset = release.Assets.FirstOrDefault();
+
+                    if (asset is null)
+                    {
+                        return null;
+                    }
+
+                    Version version = System.Version.Parse(TagStripRegex().Replace(release.TagName, string.Empty));
+
+                    return new UpdateRelease
+                    {
+                        Name = release.Name,
+                        Version = version,
+                        Summary = release.Body,
+                        PublishedAt = release.CreatedAt,
+                        DownloadUrl = asset.BrowserDownloadUrl
+                    };
+                })
+                .Where(y => y != null)
+                .Cast<UpdateRelease>()
+                .ToList()
+        };
+
+        await SendOkAsync(response, ct);
+    }
+}
--- examples/server/Services/GitHubApiService.cs
+++ examples/server/Services/GitHubApiService.cs
@@ -42,4 +42,36 @@ internal sealed class GitHubApiService(IMemoryCache memoryCache, IHostEnvironmen
 
         return release;
     }
+
+    /// <summary>
+    ///     Gets all releases of a given GitHub repository.
+    /// </summary>
+    /// <param name="owner">The username or organisation.</param>
+    /// <param name="name">The repository name.</param>
+    /// <returns>A list of <see cref="Release" />s on success or null.</returns>
+    public async Task<IEnumerable<Release>?> GetAllReleases(string owner, string name)
+    {
+        string key = $"{nameof(GitHubApiService)}-{owner}/{name}";
+
+        if (!environment.IsDevelopment())
+        {
+            if (memoryCache.TryGetValue(key, out IReadOnlyList<Release>? cached))
+            {
+                return cached;
+            }
+        }
+
+        IReadOnlyList<Release>? releases = await _gitHubClient.Repository.Release.GetAll(owner, name);
+
+        if (!environment.IsDevelopment())
+        {
+            memoryCache.Set(
+                key,
+                releases,
+                new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1) }
+            );
+        }
+
+        return releases;
+    }
 }
