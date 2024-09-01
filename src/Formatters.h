fd38b3c7aac707521236ad0451a02093843d1f80
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-09-01 20:25:58 +0200

    Replaced redundant string-ification of FS paths in logging with formatter (#76)
    * Replaced redundant string-ification of FS paths in logging with formatter use
    
    * Update appveyor.yml

--- src/Formatters.h
+++ src/Formatters.h
@@ -4,15 +4,18 @@
 #include <fmt/format.h>
 #include <filesystem>
 
-template <> struct fmt::formatter<std::filesystem::path> : fmt::formatter<std::string>
+namespace fmt
 {
-    // Parse the format string (this part is usually left unchanged)
-    template <typename ParseContext> constexpr auto parse(ParseContext& ctx) { return formatter<std::string>::parse(ctx); }
-
-    // Format the argument
-    template <typename FormatContext> auto format(const std::filesystem::path& p, FormatContext& ctx)
+    // Custom formatter for std::filesystem::path
+    template <>
+    struct formatter<std::filesystem::path> : formatter<std::string>
     {
-        // Convert the path to a string and format it
-        return formatter<std::string>::format(p.string(), ctx);
-    }
-};
+        // Parse function is inherited from formatter<std::string>
+        template <typename FormatContext>
+        auto format(const std::filesystem::path& path, FormatContext& ctx) const
+        {
+            return formatter<std::string>::format(path.string(), ctx);
+        }
+    };
+
+} // namespace fmt
