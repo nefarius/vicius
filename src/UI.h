44bd35d85052be8b1b4eb0cd23a3b1d82b2e55a1
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-12-29 16:18:27 +0100

    Added DirectXTK dependency

--- src/UI.h
+++ src/UI.h
@@ -11,5 +11,4 @@ namespace ui
     void ApplyImGuiStyleDark(float scale = 1.0f);
     void LoadFonts(HINSTANCE hInstance, float sizePixels = 16.0f, float scale = 1.0f);
     void IndeterminateProgressBar(const ImVec2& size_arg);
-    std::expected<void, HRESULT> LoadTextureFromMemory(ID3D11Device* device, const uint8_t* imageData, size_t imageSize, ID3D11ShaderResourceView** srv);
 }
