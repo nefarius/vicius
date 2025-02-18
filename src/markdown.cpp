a71dd10fc27430358474bb01179874b4f8dea0de
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-02-18 13:35:36 +0100

    Update markdown.cpp

--- src/markdown.cpp
+++ src/markdown.cpp
@@ -229,7 +229,7 @@ struct changelog : imgui_md
                 // task done (succeeded or failed)
                 if (hasFinished)
                 {
-                    auto res = task->get();
+                    std::optional<D3DResource> res = task->get();
 
                     // got our image, cache and return
                     if (res != std::nullopt)
