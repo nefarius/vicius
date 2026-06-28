4ea802310dcae72b45414e3824409215a4b9720d
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2026-06-28 22:48:18 +0200

    Update InstanceConfig.Template.cpp

--- src/InstanceConfig.Template.cpp
+++ src/InstanceConfig.Template.cpp
@@ -468,7 +468,7 @@ std::string models::InstanceConfig::RenderInjaTemplate(const std::string& tpl, c
                              const semver::version lhs = semver::version::parse(lhsValue);
                              const semver::version rhs = semver::version::parse(rhsValue);
 
-                             return util::CompareVersions(lhs, rhs) == 0;
+                             return lhs == rhs;
                          }
                          catch (const std::exception& ex)
                          {
